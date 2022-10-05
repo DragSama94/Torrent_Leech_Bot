164f7210daa72da99ea2c34aa70012cc120345a2
DRAGON SAMA <51018664+DragSama94@users.noreply.github.com>
2022-10-06 00:31:46 +0530

    Update status_message_fn.py

--- tobrot/plugins/status_message_fn.py
+++ tobrot/plugins/status_message_fn.py
@@ -62,7 +62,7 @@ async def status_message_f(client, message):
             gid_dict[chat_id].pop()
             gid_dict[chat_id].append(mess_id)
 
-    prev_mess = "By GopalSaraf"
+    prev_mess = "By Dragon Sama"
     while True:
         downloads = aria_i_p.get_downloads()
         msg = ""
